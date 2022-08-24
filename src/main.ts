378703f54457e8d39fb8a72cdf2f02e51feb0eb3
Yhomarth Reyes <yhomarth@gmail.com>
2022-08-24 12:24:04 -0400

    Obtener la geolocalización del usuario

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+
+if(!navigator.geolocation){
+  alert('No tiene permitido la geolocalizacion');
+  throw new Error('No tiene permitido la geolocalizacion');
+}
+
 if (environment.production) {
   enableProdMode();
 }
