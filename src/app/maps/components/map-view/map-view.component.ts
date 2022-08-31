24c1659a9d699ff9f84d4e341d225d367db394be
Yhomarth Reyes <yhomarth@gmail.com>
2022-08-31 18:35:23 -0400

    Marcadores y Popup

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Map, Popup, Marker } from 'mapbox-gl';
 import { PlacesService } from '../../services';
 
 @Component({
@@ -26,6 +26,18 @@ export class MapViewComponent implements AfterViewInit {
       center: this.placeService.userLocation, // starting position [lng, lat]
       zoom: 14, // starting zoom
       });
+
+    const popup = new Popup()
+      .setHTML(`
+        <h6>Aquí estoy</h6>
+        <span>Estoy en este punto del mundo</span>
+      `);
+
+      console.log(this.placeService.userLocation );
+    new Marker({ color : 'red'})
+      .setLngLat( this.placeService.userLocation )
+      .setPopup( popup )
+      .addTo( map );
     
   }
 
