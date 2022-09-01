bf591f5016c6012b587eed6d53d652fe3499df5a
Yhomarth Reyes <yhomarth@gmail.com>
2022-08-31 22:21:57 -0400

    Debounce Manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent  {
+
+
+  private debounceTimer ?: NodeJS.Timer;
+  private busqueda : string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  
+  onQueryChanged(txtQuery : string = '') {
+    
+    if( this.debounceTimer ) clearTimeout( this.debounceTimer );
+
+    this.debounceTimer = setTimeout( () => {
+
+        if((txtQuery.length > 2) && this.busqueda !== txtQuery) {
+
+            console.log('LLamar esto : ', txtQuery);
+            
+            this.busqueda = txtQuery;
+        }
+    }, 500)
+
+
   }
 
 }
